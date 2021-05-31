e0620c05ea138ab24252ea897fd22d186d75a036
Your Name <Libin>
2021-05-31 22:43:43 +0530

    background theme

--- venv/main.py
+++ venv/main.py
@@ -1,16 +1,18 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
+from kivymd.uix.button import MDRectangleFlatButton
 
 
 class DemoApp(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = 'Yellow'
+        self.theme_cls.primary_hue = '700'
+        self.theme_cls.theme_style='Dark'
         screen = Screen()
         btn_flat = MDRectangleFlatButton(text='Submit', pos_hint={'center_x': 0.5, 'center_y': 0.5})
         screen.add_widget(btn_flat)
 
-        icon_btn = MDFloatingActionButton(icon='language-python', pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        return  icon_btn
+        return  screen
 
 
 DemoApp().run()
